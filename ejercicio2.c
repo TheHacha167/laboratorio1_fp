4d8c5784942faa4e3158a5002e0eacec39730ccd
TheHacha167 <thehacha167@gmail.com>
2022-11-24 20:42:47 +0100

    Add files via upload

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,15 +1,37 @@
-/*Ejercicio 2: El programa pedirá al usuario el valor del primer número a partir del cual comenzará la impresión; y pedirá también cuántos úmeros ha de imprimir.*/
-int main(){
-    int numero[20]; // Aquí alojaremos el valor leído
-    printf("Escribe el primer número:\n");
-    scanf(numero);
+/*Ejercicio 2: El programa pedirá al usuario el valor del primer número a partir del cual 
+comenzará la impresión; y pedirá también cuántos úmeros ha de imprimir.*/
+#include  <stdio.h>
 
-   int repeticion[20]; // Aquí alojaremos el valor leído
-    printf("Escribe las veces que quieres que se repita:\n");
-    scanf(repeticion);
-    a=numero+1
-   if a>len(repeticion):
-      stop
-   int *lista(){
-      
+int main ()
+{
+    
+ 
+    int numero1;
+    int numero2;
+    
+    printf("Da un numero para el valor a comenzar \n");
+    scanf("%d",numero1); 
+    printf("Da un numero para el numero de impresiones\n");
+    scanf("%d",numero2); 
+    printf(numero1);
+ 
+    printf(numero2);
+
+    /*
+
+   int array[a];
+   for (int i = 0; i < a; ++i)
+   {
+      array[i] = i + 1;
    }
+   for (int i = 0; i < a; ++i)
+   {
+      printf("%d, ", i + 1);
+   }
+   return array;
+
+
+
+*/
+   return 0;
+}
--- ejercicio4.c
+++ ejercicio4.c
@@ -1,7 +1,9 @@
 /*Ejercicio 4: Determina si es un carácter numérico o un carácter alfabético o un espacio, e imprimir por pantalla cada cuenta según el tipo.*/
 #include <stdio.h>
 
-int main (int argc,char **argv)
-{
-   
+int main(){
+    char cadena[20]; // aquí se almacena la cadena de caracteres para luego jacer el print
+    printf("Escribe un texto:\n"); // el mensaje para saber que hace el que interactua
+    scanf("%s", cadena); // almacenamos la entrada en la cadena de caracteres 
+    printf("El texto es: %s", cadena); //hacemos la calida
 }
