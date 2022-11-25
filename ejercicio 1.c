651f6d7cc4a58a5a7e64c3319c3137d6f7833d1d
TheHacha167 <thehacha167@gmail.com>
2022-11-25 16:53:30 +0100

    cambio ejercicio 1

--- ejercicio 1.c
+++ ejercicio 1.c
@@ -1,8 +1,10 @@
 /*Ejercicio 1: Imprimir texto escrito por teclado*/
 #include <stdio.h>
+
+ char cadena[20]; // aquí se almacena la cadena de caracteres para luego jacer el print
  
 int main(){
-    char cadena[20]; // aquí se almacena la cadena de caracteres para luego jacer el print
+    
     printf("Escribe un texto:\n");// el mensaje para saber que hace el que interactua
     scanf("%s", cadena); // almacenamos la entrada en la cadena de caracteres 
     printf("El texto es: %s", cadena);//hacemos la calida
