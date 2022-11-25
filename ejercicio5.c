80daf2bc003f313dc4c2954b2e415f2a98d733e0
TheHacha167 <thehacha167@gmail.com>
2022-11-25 19:20:35 +0100

    version

--- ejercicio5.c
+++ ejercicio5.c
@@ -12,37 +12,46 @@ float potencia(float base, int exponente) {
 
 
 int main() {
-    float primero, segundo, respuesta;
-    char op;
-    int valido = 1;
+    int primer_numero, segundo_numero, resultado;
+    char tipo_de_operacion;
+    int correcto = 1;
 
-    printf("Escribe el tipo de operacion: ");
-    scanf("%c", &op);
-    printf("Primer numero de la operacion: ");
-    scanf("%f", &primero);
-    printf("Segundo numero de la operacion: ");
-    scanf("%f", &segundo);
+    printf("NO ADMITE DECIMALES");//No se admiten decimales puesto que para hacer la operacion de modulo he tenido que usar una variable int en vez de float.
+    printf("Para operar con una suma usa: +");//Le mostramos al usuario los tipos de operaciones que hay posibles.
+    printf("Para operar con una resta usa: -");//Esta linea de explicación la usamos para la resta
+    printf("Para operar con una multiplicación usa: x o *");//Al usuario le damos dos opciones para multiplicar, debido que podría usar cualquiera y si solo tenemos una 
+    printf("Para operar con un modulo usa: Modulo");
+    printf("Para operar con una division usa: /");
+    printf("Escribe el tipo de operacion: ");//Aqui enviamos una linea de texto al usuario para que escriba el tipo de operacion que quiere ejecutar.
+    scanf("%c", &tipo_de_operacion);//variable "resultado"
+    printf("Primer numero de la operacion: ");//Una vez escrito el tipo de operacion le pedimos al usuario que ingrese el primer numero para calcular, el cual guardamos en la variable "primero".
+    scanf("%f", &primer_numero);//variable "primero"
+    printf("Segundo numero de la operacion: ");//Este es el segundo valor que se guardara en la variable "segundo", el cual usara automaticamente el codigo junto a "primero" para operar.
+    scanf("%f", &segundo_numero);//variable "segudno"
 
-    switch (op) {
+    switch (tipo_de_operacion) {
         case '+':
-            respuesta = primero + segundo;
+            resultado = primer_numero + segundo_numero;
             break;
         case '-':
-            respuesta = primero - segundo;
+            resultado = primer_numero - segundo_numero;
             break;
         case '*':
         case 'x':
-            respuesta = primero * segundo;
+            resultado = primer_numero * segundo_numero;
+            break;
+        case 'Modulo':
+            resultado = primer_numero % segundo_numero;
             break;
         case '/':
-            respuesta = primero / segundo;
+            resultado = primer_numero / segundo_numero;
             break;
         default:
-            valido = 0;
+            correcto = 0;
     }
 
-    if (valido)
-        printf("El resultado final es: %f\n", respuesta);
+    if (correcto)
+        printf("El resultado final es: %f\n", resultado);
     else
         printf("Imposible de calcular.\n");
 
