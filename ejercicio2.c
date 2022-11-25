903a1ffed683a81169ced28697f1dffe713e150e
TheHacha167 <thehacha167@gmail.com>
2022-11-25 17:34:45 +0100

    Co-authored-by: tomsalsal <tomsalsal@users.noreply.github.com>

--- ejercicio2.c
+++ ejercicio2.c
@@ -21,13 +21,8 @@ int main ()
       scanf("%i", & numero2); 
 
 
-    printf("%i",numero1);
- 
-    printf("%i",numero2);
 
-
-
-      for ( int j = 0; j < numero2; j++)
+      for ( int j = 0; j < numero2; ++j)
       { 
          numero3 = numero1 * numero1; 
          printf("%i     %i     \n",numero1,numero3);
